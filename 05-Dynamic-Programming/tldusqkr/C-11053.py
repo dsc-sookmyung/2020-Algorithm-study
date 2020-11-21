78d63bef35c7b09cbf856cb6ffec524c0d86e7f9
tldusqkr <tldusqkr@gmail.com>
2020-11-21 22:02:20 +0900

    Create C-11053.py

---
+++ 05-Dynamic-Programming/tldusqkr/C-11053.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+lst = list(map(int, sys.stdin.readable().split()))
+result = [1] * n
+for i in range(1, n):
+    for j in range(i):
+        if lst[j] < lst[i]:
+            result[i] = max(result[i], result[j]+1)
+
+print(max(result))
