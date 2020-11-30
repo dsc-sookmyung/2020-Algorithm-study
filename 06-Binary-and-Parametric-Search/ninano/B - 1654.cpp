1da59d6596b5c0889ea97f94c75e49de73c9382a
NaHyun Kwon <35582991+NinanoKwon@users.noreply.github.com>
2020-11-30 23:12:18 +0900

    add week6 assignments

---
+++ 06-Binary-and-Parametric-Search/ninano/B - 1654.cpp
@@ -0,0 +1,47 @@
+//랜선 자르기
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    int arr[100000] = {0};
+    int left = 0, right = 0;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        left = max(left, arr[i]);
+    }
+    right = 1000000000 / m;
+
+    while (left + 1 < right)
+    {
+        int mid = (right + left) / 2;
+        int sum = 0, cnt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (sum + arr[i] >= mid)
+            {
+                cnt++;
+                sum = 0;
+            }
+            sum += arr[i];
+        }
+
+        if (sum != 0)
+            cnt++;
+
+        if (cnt <= m)
+            right = mid;
+        else
+            left = mid;
+    }
+
+    cout << left;
+}
